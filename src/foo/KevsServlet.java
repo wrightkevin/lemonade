c4cf994a3467737849f6ce1f428ffcf8357dcf45
kevin.wright <kevin.wright@fdmgroup.com>
2017-03-17 09:24:25 +0000

    more changes

--- src/foo/KevsServlet.java
+++ src/foo/KevsServlet.java
@@ -25,7 +25,7 @@ public class KevsServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		java.io.PrintWriter pw = response.getWriter();
 	}
 
 	/**
